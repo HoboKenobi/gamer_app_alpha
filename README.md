991c8cabfe909e749b74717873cf618dae98c82f
Nicolas Ip Yam <nicolas.ipyam@gmail.com>
2014-06-24 15:17:40 -0400

    Add a StaticPages controller

---
+++ README.md
@@ -0,0 +1,1 @@
+# Alpha phase: sample application
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def hot
+  end
+
+  def week
+  end
+
+  def free
+  end
+end
