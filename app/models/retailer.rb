dfd555aba34e5df369e71947208c66b7be7ca470
Nicolas Ip Yam <nicolas.ipyam@gmail.com>
2014-07-05 01:51:46 -0400

    trial and error for heroku

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -1,5 +1,5 @@
 class Retailer < ActiveRecord::Base
-	before_save { self.name = name.downcase }
+	#before_save { self.name = name.downcase }
 	validates :name, presence: true, length: { maximum: 20 }, 
 				     uniqueness: { case_sensitive: false }
 end
