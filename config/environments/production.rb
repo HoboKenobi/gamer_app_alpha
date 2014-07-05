190c3e48cd12a38eefc4c11b1dcb2f16d3025a2d
Nicolas Ip Yam <nicolas.ipyam@gmail.com>
2014-07-05 00:03:00 -0400

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,10 @@ GamerAppAlpha::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
